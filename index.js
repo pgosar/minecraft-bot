9dafc9d3800d0fd0d0b3b9231173877a7bc9b821
pgosar <pgosarsteam@gmail.com>
2023-01-04 16:47:33 -0600

    pathfinding fix

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 /**
- * This strategy is an advanced example of how to customize movements, place blocks, and craft items with the rg-bot package.
- * The Bot will collect coal until it has 100 points-worth of items in its inventory.
- * (Note: Coal_Ore and apples are each worth 1 point.  Why apples you say?  Apples are a possible byproduct from collecting the logs to create new pickaxes.)
- *
  * @param {RGBot} bot
  */
 function configureBot(bot) {
@@ -15,41 +11,26 @@ function configureBot(bot) {
     }
 
     async function gatherVillage() {
-        let targets = ['chest', 'poppy', 'bell']
-
+        let targets = ['chest', 'bell']
         await gatherEntity(targets)
     }
 
-
-
     // This function will make the Bot chop + pick up a named entity.
     async function gatherEntity(entityName) {
-
-        // Track whether the Bot encountered any issues while chopping.
-        // There are so many things around the spawn area that it can
-        // simply try to chop a different one
         let skipCurrentEntity = false;
         let countBefore = 0
         for (const elem of entityName) {
             countBefore += bot.getInventoryItemQuantity(elem);    
         }
-
-        // Ensure that if the Bot fails to gather the dropped item,
-        // it will try collecting another until its inventory reflects one has been picked up
         bot.chat('start search')
         let countAfter = countBefore;
         while (countAfter <= countBefore) {
             //120 reaches both villages through connecting poppies
-            const foundEntity = await bot.findBlocks({blockNames: entityName, maxDistance: 30 }).shift();
+            const foundEntity = await bot.findBlocks({blockNames: entityName, maxDistance: 120 }).shift();
             if (foundEntity) {
                 // If the Bot located one, then go chop it
                 let success = await bot.approachAndDigBlock(foundEntity.result)
-                // success = success && await bot.digBlock(foundEntity)
-                
-                
                 if (!success) {
-                    // If anything prevents the Bot from breaking the block,
-                    // then find the next-closest and try gathering that instead.
                     // skipCurrentEntity = true;
                     bot.chat('fail')
                 } else {
@@ -57,19 +38,12 @@ function configureBot(bot) {
                     bot.chat('success')
                 }
             } else {
-                // skipCurrentEntity = false;
-                // If the Bot didn't find any nearby,
-                // then allow it to wander a bit and look again.
-                // This loop makes sure it completes the 'wander' movement.
-                // let didWander = false;
-                // while (!didWander) {
-                //     didWander = await bot.wander();
-                // }
+                // when this happens, the bot has found everything in the village
+                // this makes it go to the next village
                 bot.chat('Cannot find block');
                 let dest = GoalBlock(70, 67, -110);
                 await bot.pathfinder.goto(dest);
             }
- 
             countAfter = 0
             for (const elem of entityName) {
                 countAfter += bot.getInventoryItemQuantity(elem);    
@@ -78,32 +52,15 @@ function configureBot(bot) {
         }
     }
 
-    // The bot will announce whenever it collects ore or an apple
-    bot.on('playerCollect', async (collector, collected) => {
-        const itemName = bot.getEntityName(collected).toLowerCase();
-        if (collector.username === bot.mineflayer().username && (itemName.includes('ore') || itemName === 'apple')) {
-            bot.chat(`I collected a(n) ${itemName}`);
-        }
-    });
-
     // When the Bot spawns, begin the main gathering loop.
     // Before collecting, have the Bot craft pickaxes if it has none.
     bot.on('spawn', async () => {
         bot.chat('Hello, I have arrived!');
-
-        let oreCollected = bot.getInventoryItemQuantity('coal_ore');
-        let applesCollected = bot.getInventoryItemQuantity('apple');
-
         while (true) {
             bot.chat('Looking for points');
             await gatherVillage();
-
         }
-
-        // Once the Bot has 100 points, announce it in the chat
-        bot.chat(`I reached my goal! I have ${oreCollected} coal_ore and ${applesCollected} apples`);
     });
-
 }
 
 exports.configureBot = configureBot;
